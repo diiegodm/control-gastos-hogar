9ee3c7f0b8b4f0af09b282c5c51dc33987f1077e
diiegodm <diegoarevalodm@gmail.com>
2026-07-29 16:44:44 +0200

    comida editada notas

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finanzas-hogar-v2";
+const CACHE_NAME = "finanzas-hogar-v3";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
@@ -21,6 +21,19 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put("/index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("/index.html")),
+    );
+    return;
+  }
 
   event.respondWith(
     caches.match(event.request).then((cached) => {
